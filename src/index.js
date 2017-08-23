ce9098bc0a718e6d1eb5f526722b2f5289792d1f
shumo.tokinaga <longtime1116@gmail.com>
2017-08-23 11:21:40 +0900

    Writing a Todo List Reducer (Toggling a Todo) done

--- src/index.js
+++ src/index.js
@@ -14,23 +14,56 @@ const todos = (state = [], action) => {
           completed: false
         }
       ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        } else {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+      });
     default:
       return state;
   }
 };
 
 const testAddTodo = () => {
-  const stateBefore = [];
-  const action = {
-      type: 'ADD_TODO',
-      id: 0,
-      text: 'Learn Redux'
-  };
-  const stateAfter = [{
+  const stateBefore = [
+    {
       id: 0,
       text: 'Learn Redux',
       completed: false
-  }];
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: false
+    }
+  ];
+  //  const action = {
+  //      type: 'ADD_TODO',
+  //      id: 0,
+  //      text: 'Learn Redux'
+  //  };
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: true
+    }
+  ];
 
   deepFreeze(stateBefore);
   deepFreeze(action);
