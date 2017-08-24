6f02af1c28baa2b73afbfc24599773a4c04f9dbf
shumo.tokinaga <longtime1116@gmail.com>
2017-08-24 12:16:27 +0900

    Example: Adding a Todo & Example: Toggling a Todo done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import deepFreeze from 'deep-freeze';
 import expect from 'expect';
 import './index.css';
@@ -10,7 +10,7 @@ const todo = (state, action) => {
       return {
         id: action.id,
         text: action.text,
-        completed: false
+        completed: false,
       };
     case 'TOGGLE_TODO':
       if (state.id !== action.id) {
@@ -19,20 +19,17 @@ const todo = (state, action) => {
 
       return {
         ...state,
-        completed: !state.completed
+        completed: !state.completed,
       };
     default:
       return state;
   }
-}
+};
 
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
+      return [...state, todo(undefined, action)];
     case 'TOGGLE_TODO':
       return state.map(t => todo(t, action));
     default:
@@ -40,187 +37,114 @@ const todos = (state = [], action) => {
   }
 };
 
-const visibilityFilter = (
-    state = 'SHOW_ALL',
-    action
-) => {
-    switch (action.type) {
-      case 'SET_VISIBILITY_FILTER':
-        return action.filter;
-      default:
-        return state;
-    }
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
 };
 
 const combineReducers = reducers => {
   return (state = {}, action) => {
-
     // Reduce all the keys for reducers from `todos` and `visibilityFilter`
     return Object.keys(reducers).reduce(
       (nextState, key) => {
         // Call the corresponding reducer function for a given key
-        nextState[key] = reducers[key] (
-          state[key],
-          action
-        );
+        nextState[key] = reducers[key](state[key], action);
         return nextState;
       },
-      {} // The `reduce` on our keys gradually fills this empty object until it is returned.
+      {}, // The `reduce` on our keys gradually fills this empty object until it is returned.
     );
   };
 };
 
 const todoApp = combineReducers({
   todos,
-  visibilityFilter
+  visibilityFilter,
 });
 
-//const todoApp = (state = {}, action) => {
-//  return {
-//     // Call the `todos()` reducer from last section
-//     todos: todos(
-//      state.todos,
-//      action
-//    ),
-//    visibilityFilter: visibilityFilter(
-//      state.visibilityFilter,
-//      action
-//    )
-//  };
-//};
-
-const testAddTodo = () => {
-  const stateBefore = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go Shopping',
-      completed: false
-    }
-  ];
-  //  const action = {
-  //      type: 'ADD_TODO',
-  //      id: 0,
-  //      text: 'Learn Redux'
-  //  };
-  const action = {
-    type: 'TOGGLE_TODO',
-    id: 1
-  };
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go Shopping',
-      completed: true
-    }
-  ];
-
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
-};
-
-testAddTodo();
-console.log('All tests passed')
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-const createStore = (reducer) => {
+const createStore = reducer => {
   let state;
   let listeners = [];
 
   const getState = () => state;
 
-  const dispatch = (action) => {
+  const dispatch = action => {
     state = reducer(state, action);
     listeners.forEach(listener => listener());
-  }
-  const subscribe = (listener) => {
+  };
+  const subscribe = listener => {
     listeners.push(listener);
     return () => {
       listeners = listeners.filter(l => l !== listener);
-    }
+    };
   };
 
   dispatch({}); // initialize
 
   return { getState, dispatch, subscribe };
+};
+
+const store = createStore(todoApp);
+
+const { Component } = React;
+
+let nextTodoId = 0;
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          ref={node => {
+            this.input = node;
+          }}
+        />
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: this.input.value,
+              id: nextTodoId++,
+            });
+            this.input.value = '';
+          }}
+        >
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                });
+              }}
+              style={{
+                textDecoration:
+                todo.completed ?
+                'line-through' :
+                'none'
+              }}>
+              {todo.text}
+            </li>,
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
-
-const store = createStore(counter);
-
-const Counter = ({
-  value,
-  onIncrement,
-  onDecrement
-}) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-)
-
+// See Section 8 for earlier `render()` example
 const render = () => {
   ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement = {() =>
-          store.dispatch({
-            type: 'INCREMENT'
-      })}
-      onDecrement ={() =>
-          store.dispatch({
-            type: 'DECREMENT'
-          })
-      }
-    />,
-    document.getElementById('root')
-  )
-}
+    // Render the TodoApp Component to the <div> with id 'root'
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root'),
+  );
+};
 
-// store に対して dispatch されると、呼び出されるハンドラ的なやつ
 store.subscribe(render);
 render();
-
-expect (
-  counter(0, { type: 'INCREMENT' })
-).toEqual(1);
-
-expect (
-  counter(1, { type: 'INCREMENT' })
-).toEqual(2);
-
-expect (
-  counter(2, { type: 'DECREMENT' })
-).toEqual(1);
-
-expect (
-  counter(1, { type: 'DECREMENT' })
-).toEqual(0);
-
-expect (
-    counter(1, { type: 'SOMETHING_ELSE' })
-).toEqual(1);
